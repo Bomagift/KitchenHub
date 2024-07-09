27e4b36839521e1e3dfc58bc05b516aa42b80903
boma <94929211+Bomagift@users.noreply.github.com>
2024-07-09 13:20:15 -0400

    still working

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,7 +1,11 @@
 import ProductData from "../Pages/ProductData";
 import "../Styles/Products.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const Products = () => {
+  const [showFeatured, setShowFeatured] = useState(false);
+
   const newArrivals = ProductData.filter(
     (product) => product.category === "New Arrivals"
   );
@@ -12,6 +16,10 @@ const Products = () => {
 
   const handleCart = () => {
     navigate("/cart");
+  };
+
+  const toggleFeatured = () => {
+    setShowFeatured(!showFeatured);
   };
 
   return (
@@ -43,23 +51,29 @@ const Products = () => {
       </div>
       <h2>Featured Products</h2>
       <h6>"Top Picks of the Month"</h6>
-      <div className="product-list">
-        {featuredProducts.map((product) => (
-          <div className="product-item" key={product.id}>
-            <img src={product.image} alt={product.name} />
-            <div className="love-icon">
-              <img src="Images/Group 198.png" alt="" />
-            </div>
-            <div className="add-to-cart">
-              <span onClick={handleCart}>Add to Cart</span>
-              <img src="Images/lets-icons_add-light.png" alt="" onClick={handleCart}/>
-            </div>
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-          </div>
-        ))}
+      <div className="arrow" onClick={toggleFeatured}>
+        {showFeatured ? '▲' : '▼'}
       </div>
+      {showFeatured && (
+        <div className="product-list featured">
+          {featuredProducts.map((product) => (
+            <div className="product-item" key={product.id}>
+              <img src={product.image} alt={product.name} />
+              <div className="love-icon">
+                <img src="Images/Group 198.png" alt="" />
+              </div>
+              <div className="add-to-cart">
+                <span onClick={handleCart}>Add to Cart</span>
+                <img src="Images/lets-icons_add-light.png" alt="" onClick={handleCart} />
+              </div>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
+
 export default Products;
